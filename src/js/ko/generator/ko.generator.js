109e21f112cde697b6d3b0c819348b11071462df
Thomas D'Ascoli <thomas@dasco.li>
2020-04-05 21:10:55 +0200

    overlay

--- src/js/ko/generator/ko.generator.js
+++ src/js/ko/generator/ko.generator.js
@@ -14,11 +14,6 @@ function File(){
     dataURL: ko.observable(), // FileReader.readAsDataURL(Blob|File) - The result property will contain the file/blob's data encoded as a data URL.
     base64String: ko.observable() // just the base64 string, without mime type or anything else
   });
-
-  self.fileData().base64String.subscribe(function(base64String){
-    //sendToServer(base64String);
-    console.log('base64String');
-  });
 }
 
 function Panini(uuid = uuidv4()){
@@ -26,43 +21,91 @@ function Panini(uuid = uuidv4()){
 
   self.uuid = uuid;
   self.name = ko.observable();
+  self.set = ko.observable();
   self.image = new File();
+
+  self.canvas = ko.observable();
+  //http://fabricjs.com/image-filters
+  self.canvas.subscribe(function initCanvas(element) {
+    var canvas = new fabric.Canvas(element, {
+        preserveObjectStacking: true
+    });
+
+    canvas.setWidth(300);
+    canvas.setHeight(400);
+
+    self.image.fileData().dataURL.subscribe(function(dataURL){
+      console.log('image');
+      addToCanvas(canvas, dataURL);
+    });
+
+    self.set.subscribe(function(set){
+      if (set!=undefined){
+        canvas.setOverlayImage('../assets/set/'+set.id+'.png', function() {
+          canvas.overlayImage.scaleToHeight(400);
+          canvas.renderAll();
+        });
+      }
+    });
+  });
 };
+
+function addToCanvas(canvas, dataURL){
+  fabric.Image.fromURL(dataURL, function(img) {
+      img.set({
+          id: 'img',
+          left: 0,
+          top: 0,
+          selectable: true,
+          hasBorders: false,
+          hasControls: false,
+          hasRotatingPoint: false
+      });
+      img.scaleToWidth(300);
+      canvas.add(img);
+  });
+}
+
+function Player(name, id) {
+    this.name = name;
+    this.id = id;
+};
+
+function PaniniSet(name, id) {
+    this.name = name;
+    this.id = id;
+};
+
 
 function GeneratorViewModel(){
   var self = this;
 
   self.panini = new Panini();
 
-  self.myCanvas = ko.observable();
+  self.paniniSet = ko.observable();
 
-  self.myCanvas.subscribe(function initCanvas(element) {
-    self.panini.image.fileData().dataURL.subscribe(function(dataURL){
-      console.log('image');
-      var canvas = new fabric.Canvas(element, {
-          preserveObjectStacking: true
-      });
-
-      canvas.setWidth(300);
-      canvas.setHeight(400);
-
-      fabric.Image.fromURL(dataURL, function(img) {
-          img.set({
-              id: 'img',
-              left: 0,
-              top: 0,
-              selectable: true,
-              hasBorders: false,
-              hasControls: false,
-              hasRotatingPoint: false
-          });
-          img.scaleToWidth(300);
-          canvas.add(img);
-          img.bringToFront();
-          canvas.renderAll();
-      });
-    });
+  self.panini.set.subscribe(function(set){
+    console.log('ko',set);
+    if (set!=undefined){
+      self.paniniSet('panini-sticker__'+set.id);
+    }
   });
+
+  self.set = ko.observableArray([
+            new PaniniSet('WM 1982', 'wm1982'),
+            new PaniniSet("WM 1986", 'wm1986'),
+            new PaniniSet("WM 1990", 'wm1990'),
+            new PaniniSet("WM 1994", 'wm1994'),
+            new PaniniSet("WM 2002", 'wm2002'),
+            new PaniniSet("WM 2014", 'wm2014'),
+            new PaniniSet("WM 2018", 'wm2018')
+        ]);
+
+  self.players = ko.observableArray([
+            new Player("UK", 65000000),
+            new Player("USA", 320000000),
+            new Player("Sweden", 29000000)
+        ]);
 
   self.save = function(){
     var panini = {
